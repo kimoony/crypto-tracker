68261d65510db115542e18da639a31b5ade5f4d0
kimoony <hoonby9011@gmail.com>
2022-03-20 04:53:00 +0900

    created crypto-tracker and setup

---
+++ src/routes/Coin.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { useParams } from 'react-router-dom'
+
+interface RouteParams {
+  coinId: string
+}
+
+function Coin() {
+  const { coinId } = useParams<RouteParams>()
+
+  return (
+    <div>Coin: {coinId}</div>
+  )
+}
+
+export default Coin
---
+++ src/routes/Coins.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import styled from 'styled-components';
+
+
+function Coins() {
+  return (
+    <div>Coins</div>
+  )
+}
+
+export default Coins
